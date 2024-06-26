060cfe2b431f495168c4256ad59158569232a3fc
prateek sharma <ppprateeksharma006@gmail.com>
2024-06-26 14:41:15 +0530

    changes saved

--- client/lib/core/constants/server_constant.dart
+++ client/lib/core/constants/server_constant.dart
@@ -2,5 +2,5 @@ import 'dart:io';
 
 class ServerConstant {
   static String serverURL =
-      Platform.isAndroid ? 'https://flutter-spotify-clone.onrender.com' : 'http://127.0.0.1:8000';
+      Platform.isAndroid ? 'http://192.168.0.229:8000' : 'http://127.0.0.1:8000';
 }
