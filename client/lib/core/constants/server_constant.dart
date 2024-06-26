503c09b8dc9fefcfcd0967da126d9f5fae955721
prateek sharma <ppprateeksharma006@gmail.com>
2024-06-26 13:45:47 +0530

    jwt error check

---
+++ client/lib/core/constants/server_constant.dart
@@ -0,0 +1,6 @@
+import 'dart:io';
+
+class ServerConstant {
+  static String serverURL =
+      Platform.isAndroid ? 'https://flutter-spotify-clone.onrender.com' : 'http://127.0.0.1:8000';
+}
---
+++ server/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = 'postgresql://postgres:Prateek006@localhost:5432/musicapp'
+#DATABASE_URL = "postgresql://musicapp_gy3r_user:KFugeM6IFckXYzcNGDlPfCsvXwSjwijX@dpg-cptr5308fa8c738p38o0-a/musicapp_gy3r"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
